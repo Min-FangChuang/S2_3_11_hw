58f3521c63d7ba763303c24c9577e41a562aeb52
minfang <a0984308118@gmail.com>
2024-03-24 15:05:11 +0800

    <style> formating hw4.cpp, hw5.cpp
    add the program information and footnotes

--- hw4.cpp
+++ hw4.cpp
@@ -1,11 +1,20 @@
+/*Programming: Min-Fang Chuang   Date: 2024/03/23
+ *Instruction: Chow-Sing Lin     Class: Program Design
+ *
+ *calculate the distance of user and reviewer, and predict the rating of user
+ */
 #include<iostream>
+#include<cstdlib>
 #include<cmath>
 using namespace std;
 
 int main() {
+	//table of reviewer-rating data
 	int data[4][6] = { {3,1,5,2,1,5},{4,2,1,4,2,4},{3,1,2,4,4,1},{5,1,4,2,4,2} };
+	//user's ratings for any three movies
 	int user[3][2];
 
+	//get the user's ratings
 	cout << "Enter ratings for any three movies:\n";
 	for (int i = 0;i < 3;i++) {
 		cout << "Movie"<<i+1<<" (100~105) : ";
@@ -15,6 +24,7 @@ int main() {
 		cout << endl;
 	}
 
+	//compute the distance of user and reviewer
 	int clost = 10, clost_viewer;
 	for (int i = 0;i < 4;i++) {
 		int difference = 0;
@@ -29,9 +39,11 @@ int main() {
 		}
 	}
 
-
+	//display the closest match reviewer
 	cout << "\nThe cloeset match is reviewer " << clost_viewer << endl;
 	cout << "The distance is " << clost << endl;
+
+	//predict ratings of other three movies
 	cout << "\nPredict" << endl;
 	for (int i = 0;i < 6;i++) {
 		bool have_rate = false;
@@ -46,4 +58,6 @@ int main() {
 		}
 	}
 
+	system("pause");
+	return 0;
 }
--- hw5.cpp
+++ hw5.cpp
@@ -1,14 +1,23 @@
+/*Programming: Min-Fang Chuang   Date: 2024/03/23
+ *Instruction: Chow-Sing Lin     Class: Program Design
+ *
+ *Simulate the authentication process
+ */
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
 using namespace std;
 
 int main() {
+	//the actual pin
 	int actual_PIN[5] = { 1,2,3,4,5 };
+	//random numbers to the digits from 0 to 9
 	int rand_dig[10];
 
+	//set random seed
 	srand(time(NULL));
 
+	//display the random digits
 	cout << "PIN: 0 1 2 3 4 5 6 7 8 9" << endl;
 	cout << "NUM:";
 	for (int i = 0;i < 10;i++) {
@@ -16,11 +25,13 @@ int main() {
 		cout <<" " << rand_dig[i];
 	}
 
-	int password;
+	//response from user
+	int password;  //input - user enter password
 	cout << "\n\n";
 	cout << "Enter the password: ";
 	cin >> password;
 
+	//identify whether the password is correct
 	bool correct = true;
 	for (int i = 0;i < 5;i++) {
 		int digit;
@@ -32,6 +43,7 @@ int main() {
 		}
 	}
 
+	//display the result
 	if (correct) {
 		cout << "The password is correct!!" << endl;
 	}
@@ -39,5 +51,6 @@ int main() {
 		cout << "The password isn't correct, please try again." << endl;
 	}
 
+	system("pause");
 	return 0;
 }
